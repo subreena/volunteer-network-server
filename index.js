76f944ac1f716d6a731d9a84988ae9ec2b53ad63
Subreena <subreena20@gmail.com>
2020-10-06 09:13:19 +0600

    cors enable

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ const client = new MongoClient(uri, {
     useUnifiedTopology: true
 });
 const app = express()
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
+// enable cors
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 
